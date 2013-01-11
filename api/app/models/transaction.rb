2fb2df79f4e098cebf3685e39b9a0263717ce178
austingulati <contact@austingulati.com>
2013-01-11 17:37:10 -0500

    Add relationships

--- api/app/models/transaction.rb
+++ api/app/models/transaction.rb
@@ -5,5 +5,8 @@ class Transaction < ActiveRecord::Base
   validates :receiver_id, :presence => true
   validates :sender_id, :presence => true
   validates :status, :presence => true, :numericality => { :only_integer => true }, :inclusion => { :in => 0..2 }
+
+  belongs_to :sender, :class_name => "User"
+  belongs_to :receiver, :class_name => "User"
   
 end
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   validates :password, :length => {:minimum => 6}
   validates :email, :uniqueness => true, :format => {:with => /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/}
   validates :balance, :numericality => {:only_integer => true}
+
+  has_many :sent_transactions, class_name: "Transaction", foreign_key: "sender_id"
+  has_many :received_transactions, class_name: "Transaction", foreign_key: "receiver_id"
 end
