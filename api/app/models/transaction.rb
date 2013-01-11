4da5c1b007c9c09a5e537916c6b334f81b793b74
austingulati <contact@austingulati.com>
2013-01-11 17:35:13 -0500

    Add validations for transaction

--- api/app/models/transaction.rb
+++ api/app/models/transaction.rb
@@ -1,3 +1,9 @@
 class Transaction < ActiveRecord::Base
   attr_accessible :amount, :receiver_id, :sender_id, :status
+
+  validates :amount, :presence => true, :numericality => { :only_integer => true }
+  validates :receiver_id, :presence => true
+  validates :sender_id, :presence => true
+  validates :status, :presence => true, :numericality => { :only_integer => true }, :inclusion => { :in => 0..2 }
+  
 end
