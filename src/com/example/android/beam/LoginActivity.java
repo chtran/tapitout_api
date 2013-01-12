f08c7e03cf29762322357dd58bb0c2547c49c36f
Austin Gulati <contact@austingulati.com>
2013-01-11 22:00:22 -0500

    Set default to LoginActivity

---
+++ src/com/example/android/beam/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.beam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+}
