ae1f2946ebe84d85c24dd20725f05c1e4c1cf25f
Austin Gulati <contact@austingulati.com>
2013-01-11 22:58:44 -0500

    Logging in works!

---
+++ src/com/example/android/beam/SignUpActivity.java
@@ -0,0 +1,12 @@
+package com.example.android.beam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUpActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+}
