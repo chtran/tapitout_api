63983b669e2a5cbc80e919eb6119342590f17f4b
Austin Gulati <contact@austingulati.com>
2013-01-12 02:44:11 -0500

    Initial commit

---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    # sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobarbaz"
+    password_confirmation "foobarbaz"
+  end
+end
