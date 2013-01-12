43863eb49a8dacfb4cfd8c54fe7a99c4a732c56d
Austin Gulati <contact@austingulati.com>
2013-01-12 00:32:32 -0500

    Update images, start on TransactionActivity

--- gen/com/example/android/beam/R.java
+++ gen/com/example/android/beam/R.java
@@ -19,21 +19,21 @@ public final class R {
         public static final int ic_menu_preferences=0x7f020001;
     }
     public static final class id {
-        public static final int confirmpass=0x7f070005;
-        public static final int email=0x7f070002;
+        public static final int confirmpass=0x7f070004;
+        public static final int email=0x7f070001;
         public static final int menu_settings=0x7f070006;
-        public static final int pass=0x7f070004;
-        public static final int password=0x7f070003;
-        public static final int textView=0x7f070001;
-        public static final int transactionAmount=0x7f070000;
+        public static final int pass=0x7f070003;
+        public static final int password=0x7f070002;
+        public static final int textView=0x7f070000;
+        public static final int transactionAmount=0x7f070005;
     }
     public static final class layout {
         public static final int confirmation=0x7f030000;
-        public static final int inquiry=0x7f030001;
-        public static final int login=0x7f030002;
-        public static final int menu=0x7f030003;
-        public static final int secure=0x7f030004;
-        public static final int signup=0x7f030005;
+        public static final int login=0x7f030001;
+        public static final int menu=0x7f030002;
+        public static final int secure=0x7f030003;
+        public static final int signup=0x7f030004;
+        public static final int transaction=0x7f030005;
         public static final int waiting=0x7f030006;
         public static final int welcome=0x7f030007;
     }
