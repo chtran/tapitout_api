8522df8bcbdefedc3a35ca614ea03070d6b819dd
Chau Tran <chau_tran@brown.edu>
2013-01-11 20:02:08 -0800

    Added balance, name to user

--- gen/com/example/android/beam/R.java
+++ gen/com/example/android/beam/R.java
@@ -10,23 +10,45 @@ package com.example.android.beam;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int white=0x7f050000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int ic_menu_preferences=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f060001;
-        public static final int textView=0x7f060000;
+        public static final int confirmpass=0x7f070004;
+        public static final int menu_settings=0x7f070005;
+        public static final int pass=0x7f070003;
+        public static final int password=0x7f070002;
+        public static final int textView=0x7f070000;
+        public static final int username=0x7f070001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int confirmation=0x7f030000;
+        public static final int inquiry=0x7f030001;
+        public static final int login=0x7f030002;
+        public static final int menu=0x7f030003;
+        public static final int secure=0x7f030004;
+        public static final int signup=0x7f030005;
+        public static final int waiting=0x7f030006;
+        public static final int welcome=0x7f030007;
     }
     public static final class menu {
-        public static final int options=0x7f050000;
+        public static final int options=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int button_addcard=0x7f040003;
+        public static final int button_quit=0x7f040005;
+        public static final int button_transaction=0x7f040004;
+        public static final int cpassword=0x7f04000a;
         public static final int info=0x7f040001;
+        public static final int login=0x7f040008;
         public static final int menu_settings=0x7f040002;
+        public static final int password=0x7f040006;
+        public static final int signup=0x7f040009;
+        public static final int username=0x7f040007;
     }
 }
