49330b5018bac92cac99f16a5d85dd83e952f196
Austin Gulati <contact@austingulati.com>
2013-01-12 00:05:59 -0500

    Change to android-17

--- gen/com/example/android/beam/R.java
+++ gen/com/example/android/beam/R.java
@@ -19,12 +19,13 @@ public final class R {
         public static final int ic_menu_preferences=0x7f020001;
     }
     public static final class id {
-        public static final int confirmpass=0x7f070004;
-        public static final int email=0x7f070001;
-        public static final int menu_settings=0x7f070005;
-        public static final int pass=0x7f070003;
-        public static final int password=0x7f070002;
-        public static final int textView=0x7f070000;
+        public static final int confirmpass=0x7f070005;
+        public static final int email=0x7f070002;
+        public static final int menu_settings=0x7f070006;
+        public static final int pass=0x7f070004;
+        public static final int password=0x7f070003;
+        public static final int textView=0x7f070001;
+        public static final int transactionAmount=0x7f070000;
     }
     public static final class layout {
         public static final int confirmation=0x7f030000;
@@ -42,15 +43,24 @@ public final class R {
     public static final class string {
         public static final int api_base=0x7f04000b;
         public static final int app_name=0x7f040000;
+        public static final int balance=0x7f04000c;
         public static final int button_addcard=0x7f040003;
         public static final int button_quit=0x7f040005;
         public static final int button_transaction=0x7f040004;
+        public static final int cancel=0x7f04000f;
         public static final int cpassword=0x7f04000a;
         public static final int email=0x7f040007;
+        public static final int hint=0x7f040011;
+        public static final int howMuch=0x7f04000d;
         public static final int info=0x7f040001;
         public static final int login=0x7f040008;
         public static final int menu_settings=0x7f040002;
+        public static final int money=0x7f040010;
+        public static final int no=0x7f040013;
+        public static final int ok=0x7f04000e;
         public static final int password=0x7f040006;
+        public static final int question1=0x7f040014;
         public static final int signup=0x7f040009;
+        public static final int yes=0x7f040012;
     }
 }
